32e1db12e4c5cedf4769a554c7dfdd0bb6f1cabd
Alex <a.boulom@it-students.fr>
2023-10-01 14:41:26 +0200

    ✨ feat(app): fixing controller and entity for schedule

--- src/Controller/ElectrovalveCreationController.php
+++ src/Controller/ElectrovalveCreationController.php
@@ -30,46 +30,64 @@ class ElectrovalveCreationController
         $user = $this->tokenStorage->getToken()->getUser();
         $this->logger->info('Request Body:', ['data' => $data]);
 
+        // Validation des champs obligatoires
+        if (empty($data->name) || $data->position === null) {
+            throw new \InvalidArgumentException("Les champs name et position sont obligatoires.");
+        }
+
+        // Validation des heures
+        foreach ($data->schedules as $scheduleData) {
+            if ($scheduleData['hourStart'] >= $scheduleData['hourEnd']) {
+                throw new \InvalidArgumentException("L'heure de début doit être inférieure à l'heure de fin.");
+            }
+            if ($scheduleData['hourStart'] < 0 || $scheduleData['hourStart'] > 24 || $scheduleData['hourEnd'] < 0 || $scheduleData['hourEnd'] > 24) {
+                throw new \InvalidArgumentException("Les heures fournies doivent être entre 0 et 24.");
+            }
+        }
+
+        // Validation des jours
+        $validDays = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+        foreach ($data->schedules as $scheduleData) {
+            foreach ($scheduleData['day'] as $day) {
+                if (!in_array($day, $validDays)) {
+                    throw new \InvalidArgumentException("Jour invalide : $day");
+                }
+            }
+            if (count($scheduleData['day']) !== count(array_unique($scheduleData['day']))) {
+                throw new \InvalidArgumentException("Des jours en double ont été fournis.");
+            }
+        }
+
+        // Validation de la position et de la durée
+        if ($data->position < 0) {
+            throw new \InvalidArgumentException("La position doit être un nombre positif.");
+        }
+        if ($data->duration !== null && $data->duration < 0) {
+            throw new \InvalidArgumentException("La durée doit être un nombre positif.");
+        }
+
+        // Création de l'électrovanne
         $electrovalve = new Electrovalve();
+        $electrovalve->setName($data->name)
+            ->setPosition($data->position)
+            ->setIsAutomatic($data->isAutomatic)
+            ->setUser($user);
 
-        if ($data->name !== null) {
-            $electrovalve->setName($data->name);
-        }
-
-        if ($data->position !== null) {
-            $electrovalve->setPosition($data->position);
-        }
-
-        if ($data->isAutomatic !== null) {
-            $electrovalve->setIsAutomatic($data->isAutomatic);
-        }
-
-        $electrovalve->setUser($user); // Associer l'utilisateur à l'Electrovalve
-
+        // Création des paramètres de l'électrovanne
         $valveSettings = new ValveSettings();
-
-        if ($data->rainThreshold !== null) {
-            $valveSettings->setRainThreshold($data->rainThreshold);
-        }
-
-        if ($data->moistureThreshold !== null) {
-            $valveSettings->setMoistureThreshold($data->moistureThreshold);
-        }
-
-        if ($data->duration !== null) {
-            $valveSettings->setDuration($data->duration);
-        }
-
-        $valveSettings->setElectrovalve($electrovalve);
+        $valveSettings->setRainThreshold($data->rainThreshold)
+            ->setMoistureThreshold($data->moistureThreshold)
+            ->setDuration($data->duration)
+            ->setElectrovalve($electrovalve);
 
         $electrovalve->setValveSettings($valveSettings);
 
+        // Création des horaires
         foreach ($data->schedules as $scheduleData) {
             $schedule = new Schedule();
-            $schedule
-                ->setHourStart($scheduleData['hourStart'])
+            $schedule->setHourStart($scheduleData['hourStart'])
                 ->setHourEnd($scheduleData['hourEnd'])
-                ->setDay($scheduleData['day'])
+                ->setDays($scheduleData['day'])
                 ->setIsActivated($scheduleData['isActivated'])
                 ->setValveSettings($valveSettings);
             $valveSettings->addSchedule($schedule);
@@ -80,5 +98,4 @@ class ElectrovalveCreationController
 
         return $electrovalve;
     }
-
 }
